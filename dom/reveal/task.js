b5c1eb38d836fb3e27f18928dc014fc649c7291e
Alex Smith <alex.vesson.smith@gmail.com>
2020-10-28 23:13:02 +0300

    fixed 1

--- dom/reveal/task.js
+++ dom/reveal/task.js
@@ -2,15 +2,14 @@
 
 document.addEventListener("scroll", () => {
   const reveal = Array.from(document.querySelectorAll(".reveal"));
-  console.log(reveal);
   const viewportHeight = window.innerHeight;
-  reveal.forEach (item => {
+  reveal.forEach(item => {
     let rect = item.getBoundingClientRect();
-    console.log(rect.top);
-    if (rect.bottom < viewportHeight){
-      item.classList.add("reveal_active");
-    } else if (rect.bottom > viewportHeight) {
+    console.log(rect.bottom);
+    if (rect.top < 0 || rect.bottom > viewportHeight) {
       item.classList.remove("reveal_active");
+    } else if (rect.bottom < viewportHeight) {
+      item.classList.add("reveal_active");
     }
   });
 })
